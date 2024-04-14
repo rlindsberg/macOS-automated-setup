88eb444ba3905555c30a7a9145b8e0bb108cdcb4
Roderick Karlemstrand <roderick@karlemstrand.com>
2024-04-14 18:53:33 +0200

    remove sudo, add more
    https://apple.stackexchange.com/questions/456623/macos-command-line-for-track-pad-settings

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-sudo defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
